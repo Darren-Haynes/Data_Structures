a12d5ca55d6b88acab51bd04d98b0762565ea4c2
Darren-Haynes <darrenhaynes@zoho.com>
2017-11-25 14:59:26 -0800

    updated variables

--- src/conftest.py
+++ src/conftest.py
@@ -5,15 +5,15 @@ import pytest
 @pytest.fixture
 def empty_g():
     """Create an empty graph."""
-    from simple_graph import SimpleGraph
-    return SimpleGraph()
+    from weighted_graph import WeightedGraph
+    return WeightedGraph()
 
 
 @pytest.fixture
 def non_ref():
     """Create simple non referential graph."""
-    from simple_graph import SimpleGraph
-    empty_g = SimpleGraph()
+    from weighted_graph import WeightedGraph
+    empty_g = WeightedGraph()
     empty_g.add_edge('A', 'B')
     empty_g.add_edge('A', 'C')
     empty_g.add_edge('B', 'D')
@@ -26,8 +26,8 @@ def non_ref():
 @pytest.fixture
 def cyclic():
     """Create simple cyclic graph."""
-    from simple_graph import SimpleGraph
-    empty_g = SimpleGraph()
+    from weighted_graph import WeightedGraph
+    empty_g = WeightedGraph()
     empty_g.add_edge('A', 'B')
     empty_g.add_edge('B', 'C')
     empty_g.add_edge('C', 'A')
