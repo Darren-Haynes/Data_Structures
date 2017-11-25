4bf73283079ad4f04b6aa70994e65c6d8b6dbd39
Darren-Haynes <darrenhaynes@zoho.com>
2017-11-25 14:06:33 -0800

    edges returns tuples, KeyErrors now ValueErrors

--- src/simple_graph.py
+++ src/simple_graph.py
@@ -35,7 +35,7 @@ class SimpleGraph(object):
             for edge in edges:
                 if edge not in all_edges:
                     all_edges.append(edge)
-        return all_edges
+        return tuple(all_edges)
 
     def del_node(self, val):
         """If node in graph, delete it."""
@@ -46,7 +46,7 @@ class SimpleGraph(object):
                     self.graph[loop_val].remove(val)
             del self.graph[val]
         except KeyError:
-            raise KeyError("Node doesn't exist")
+            raise ValueError("Node doesn't exist")
 
     def del_edge(self, val, edge):
         """Remove connection between node and edge if edge exists."""
@@ -58,7 +58,7 @@ class SimpleGraph(object):
             except ValueError:
                 raise ValueError("Edge doesn't exist")
         except KeyError:
-            raise KeyError("Node doesn't exist")
+            raise ValueError("Node doesn't exist")
 
     def has_node(self, val):
         """If node exists return True."""
@@ -67,14 +67,14 @@ class SimpleGraph(object):
     def neighbors(self, val):
         """Return all edges of a node."""
         if val not in self.graph:
-            raise KeyError("Node doesn't exist.")
+            raise ValueError("Node doesn't exist.")
 
         return self.graph[val]
 
     def adjacent(self, val1, val2):
         """Return if one node has edge with the other."""
         if val1 not in self.graph or val2 not in self.graph:
-            raise KeyError("Node doesn't exist.")
+            raise ValueError("Node doesn't exist.")
 
         return val2 in self.graph[val1] or val1 in self.graph[val2]
 
