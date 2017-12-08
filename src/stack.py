aed2b1e801330eaeacea2389f7908a511311995b
Darren-Haynes <darrenhaynes@zoho.com>
2017-12-08 12:27:09 -0800

    initial commit of stack

---
+++ src/stack.py
@@ -0,0 +1,19 @@
+"""Stack Data Structure."""
+
+from linked_list import LinkedList
+
+
+class Stack(object):
+    """The Stack Object."""
+
+    def __init__(self, itr=()):
+        """Instantiate an empty stack."""
+        self.stack = LinkedList(itr=())
+
+    def push(self, val):
+        """Push a value into the stack."""
+        self.stack.push(val)
+
+    def pop(self):
+        """Pop last value pushed into stack, if stack not empty."""
+        return self.stack.pop()
