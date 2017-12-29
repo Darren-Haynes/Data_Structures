36344a489cbbcbaec475615730c91f0257673a3b
Darren-Haynes <darrenhaynes@zoho.com>
2017-12-29 15:33:08 -0800

    Added 'if name block' that times sorting.

--- src/bubble.py
+++ src/bubble.py
@@ -1,5 +1,6 @@
 """Implement a bubble sort algorithm."""
 
+import timeit
 
 def bubble_sort(arr):
     """Sort iterable from smallest to largest."""
@@ -9,3 +10,10 @@ def bubble_sort(arr):
             if arr[i] > arr[i + 1]:
                 arr[i], arr[i + 1] = arr[i + 1], arr[i]
     return arr
+
+if __name__ == '__main__':
+    user_input = input('Input comma seperated numbers: ')
+    input_list = user_input.split(',')
+    numbers = [int(i) for i in input_list]
+    print(bubble_sort(numbers))
+    print('Time: {}'.format(timeit.timeit("bubble_sort(numbers)", setup="from __main__ import bubble_sort, numbers")))
