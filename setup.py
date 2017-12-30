ce239ace03b8761236aa0751e2715c9af03f1a82
Darren-Haynes <darrenhaynes@zoho.com>
2017-12-30 13:13:29 -0800

    updated setup.py to reflect all data-structures.

--- setup.py
+++ setup.py
@@ -2,18 +2,8 @@
 from setuptools import setup
 
 setup(
-<<<<<<< HEAD
-    name='Insertion Sort',
-    description='Insertion sort algorithm',
-=======
-<<<<<<< HEAD
-    name='Bubble Sort',
-    description='Bubble sort algorithm',
-=======
     name='data-structures',
     description='Data structure written in Python',
->>>>>>> fa0b3a1b2778a018384d6f762a969d0d025c7c4a
->>>>>>> 713c0ddff52ac83cd9eb0aed9f797312b5a708a8
     package_dir={'': 'src'},
     author='Darren Haynes',
     author_email='dummy-email@zoho.com',
@@ -25,10 +15,6 @@ setup(
     },
     entry_points={
         'console_scripts': {
-<<<<<<< HEAD
-            'bubble=bubble:main'
-=======
->>>>>>> fa0b3a1b2778a018384d6f762a969d0d025c7c4a
         }
     }
 )
